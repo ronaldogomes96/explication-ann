538c03f7eaad62b435638088dcd038a074e6e721
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-08-26 15:17:09 +0100

    transform x using StandardScaler

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+
+
+def transform(x, columns):
+    x = StandardScaler().fit_transform(x)
+    return pd.DataFrame(x, columns=columns)
+
 
 if __name__ == '__main__':
     df = pd.read_csv('iris.csv')
     features, target = df.columns[:-1], df.columns[-1]
     x, y = df.loc[:, features], df.loc[:, target]
+    x = transform(x, features)
     print(x)
     print(y)

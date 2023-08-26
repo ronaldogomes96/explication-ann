0e7600246546643d0e26470b365cb2c0036015a9
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-08-26 14:28:19 +0100

    load iris dataset from CSV

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    df = pd.read_csv('iris.csv')
+    features, target = df.columns[:-1], df.columns[-1]
+    x, y = df.loc[:, features], df.loc[:, target]
+    print(x)
+    print(y)

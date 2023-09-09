ef92b773fb7e9ca66507f06ea5564400d38b2965
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-09-09 17:52:51 +0100

    check if the model was already trained

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 from src.datasets.utils import is_dataset_prepared, prepare_and_save_dataset, read_all_datasets
-from src.models.utils import eval, train
+from src.models.utils import eval, train, is_model_trained
 
 if __name__ == '__main__':
     Path('log').mkdir(exist_ok=True)
@@ -19,5 +19,6 @@ if __name__ == '__main__':
         logging.info(f'Preparing the dataset {dataset_name}...')
         prepare_and_save_dataset(dataset_name)
     (x_train, y_train), (x_val, y_val), (x_test, y_test) = read_all_datasets(dataset_name)
-    train(dataset_name, x_train, y_train, x_val, y_val)
+    if not is_model_trained(dataset_name):
+        train(dataset_name, x_train, y_train, x_val, y_val)
     eval(dataset_name, x_test, y_test)

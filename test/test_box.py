98e614f1a6b6ca7ff02fcb1c86376bcaa29becd3
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-09-24 22:13:03 +0100

    add unit test for box_has_solution

--- test/test_box.py
+++ test/test_box.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from numpy.testing import assert_array_equal
 
-from src.explications.box import box_relax_input_to_bounds, box_forward, box_check_solution
+from src.explications.box import box_relax_input_to_bounds, box_forward, box_check_solution, box_has_solution
 
 
 class Layer:
@@ -74,6 +74,64 @@ class TestBox(unittest.TestCase):
             (-2.5913954, -0.90065571)
         )
         self.assertFalse(box_check_solution(output_bounds, network_output))
+
+    def test_box_has_solution(self):
+        layers = (
+            Layer(
+                weights=(
+                    (0.07841454, -0.8021213, -0.5349561),
+                    (-0.872978, -0.7981068, -0.9043029),
+                    (1.2508274, -0.6652787, -0.32324892),
+                    (2.4814022, 0.03720671, -0.84925544)
+                ),
+                biases=(-0.40919042, 0.0, 0.0)
+            ),
+            Layer(
+                weights=(
+                    (2.7906456, 0.8087422, 0.80422074),
+                    (-0.68973327, 0.34983993, -0.826509),
+                    (-0.7777126, -0.07366228, -0.05996084)
+                ),
+                biases=(-0.0019502, -1.0573266, -1.0513506)
+            ),
+            Layer(
+                weights=(
+                    (-3.3824484, 1.3298496, 0.5923862),
+                    (-0.08571004, -4.973834, 3.4073255),
+                    (0.26439068, -4.436257, 3.1741834)
+                ),
+                biases=(5.100699, -0.4697151, -2.5913954)
+            ),
+        )
+        network_output = 0
+        input_bounds = (
+            (0.0, 1.0),
+            (0.625, 0.625),
+            (0.0677966101694915, 0.0677966101694915),
+            (0.0416666666666666, 0.0416666666666666)
+        )
+        self.assertTrue(box_has_solution(input_bounds, layers, network_output))
+        input_bounds = (
+            (0.0, 1.0),
+            (0.0, 1.0),
+            (0.0677966101694915, 0.0677966101694915),
+            (0.0416666666666666, 0.0416666666666666)
+        )
+        self.assertTrue(box_has_solution(input_bounds, layers, network_output))
+        input_bounds = (
+            (0.0, 1.0),
+            (0.0, 1.0),
+            (0.0, 1.0),
+            (0.0416666666666666, 0.0416666666666666)
+        )
+        self.assertFalse(box_has_solution(input_bounds, layers, network_output))
+        input_bounds = (
+            (0.0, 1.0),
+            (0.0, 1.0),
+            (0.0677966101694915, 0.0677966101694915),
+            (0.0, 1.0)
+        )
+        self.assertFalse(box_has_solution(input_bounds, layers, network_output))
 
 
 if __name__ == '__main__':

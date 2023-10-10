89eb1dcc8d849a33f46879a1257316b9d848b458
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-10-10 22:04:35 +0100

    add dataset mnist

---
+++ src/datasets/mnist/mnist.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from os.path import dirname, join
+
+
+def load_dataset(transform_x_fn=None):
+    dataset_path = join(dirname(__file__), 'mnist.csv')
+    df = pd.read_csv(dataset_path)
+    features, target = df.columns[1:], df.columns[0]
+    x, y = df.loc[:, features], df.loc[:, target]
+    if transform_x_fn:
+        x = transform_x_fn(x, features)
+    return x, y

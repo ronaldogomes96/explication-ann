f900a01d8c5b24ac77684c20fa02f7cca436dc53
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-10-19 20:54:12 +0100

    rename mdl_clone to mdl

--- src/explications/utils.py
+++ src/explications/utils.py
@@ -41,22 +41,22 @@ def minimal_explication(mdl: Model, layers, bounds, network, metrics, log_output
         logging.info('--------------------------------------------------------------------------------')
         logging.info(f'>>> INPUT\n{network["input"]}')
         logging.info(f'>>> OUTPUT\n{network["output"]}')
-    mdl_clone = mdl.clone(new_name='clone')
+    mdl = mdl.clone(new_name='clone')
     number_features = len(bounds['input'])
     number_outputs = len(bounds['output'])
     variables = {
-        'input': [mdl_clone.get_var_by_name(f'x_{feature_index}') for feature_index in range(number_features)],
-        'output': [mdl_clone.get_var_by_name(f'o_{output_index}') for output_index in range(number_outputs)],
-        'binary': mdl_clone.binary_var_list(number_outputs - 1, name='q')
+        'input': [mdl.get_var_by_name(f'x_{feature_index}') for feature_index in range(number_features)],
+        'output': [mdl.get_var_by_name(f'o_{output_index}') for output_index in range(number_outputs)],
+        'binary': mdl.binary_var_list(number_outputs - 1, name='q')
     }
-    input_constraints = mdl_clone.add_constraints(
+    input_constraints = mdl.add_constraints(
         [input_variable == feature for input_variable, feature in zip(variables['input'], network['input'])])
-    mdl_clone.add_constraint(mdl_clone.sum(variables['binary']) >= 1)
-    insert_tjeng_output_constraints(mdl_clone, bounds['output'], variables, network['output'])
+    mdl.add_constraint(mdl.sum(variables['binary']) >= 1)
+    insert_tjeng_output_constraints(mdl, bounds['output'], variables, network['output'])
     explication_mask = np.ones_like(network['input'], dtype=bool)
     box_mask = np.zeros_like(network['input'], dtype=bool)
     for constraint_index, constraint in enumerate(input_constraints):
-        mdl_clone.remove_constraint(constraint)
+        mdl.remove_constraint(constraint)
         explication_mask[constraint_index] = False
         if use_box:
             start_time = time()
@@ -68,11 +68,11 @@ def minimal_explication(mdl: Model, layers, bounds, network, metrics, log_output
                 box_mask[constraint_index] = True
                 metrics['with_box']['calls_to_box'] += 1
                 continue
-        mdl_clone.solve(log_output=False)
-        if mdl_clone.solution is not None:
-            mdl_clone.add_constraint(constraint)
+        mdl.solve(log_output=False)
+        if mdl.solution is not None:
+            mdl.add_constraint(constraint)
             explication_mask[constraint_index] = True
-    mdl_clone.end()
+    mdl.end()
     if log_output:
         logging.info('>>> EXPLICATION')
         logging.info(f'- Relevant: {list(network["features"][explication_mask])}')
